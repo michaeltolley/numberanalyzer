e4ec1e82e0b2110e8e15520f665eb3838e230433
Michael Tolley <michael@tolleyfam.com>
2021-02-11 13:55:56 -0800

    added average and functions

--- myNumberAnalyzer.py
+++ myNumberAnalyzer.py
@@ -1,10 +1,29 @@
-numbers = [1,2,4,4,5]
+def getSum(numbers):
 
-sum = 0
+    sum = 0
 
-for number in numbers:
-    print(number)
+    for number in numbers:
+        print(number)
+        sum = sum + number
 
-    sum = sum + number
+    return sum
+
+def getAverage(numbers):
+    sum = getSum(numbers)
+    average = sum / len(numbers)
+    return average
+
+
+numbers = [1,2,3,4,5,31,25,43,13,25]
+moreNumbers = [123,2133,123,12,3344,12,1239,489]
+
+average = getAverage(numbers)
+sum = getSum(numbers)
+
+moreAverage = getAverage(moreNumbers)
 
 print ("sum equals " + str(sum) )
+
+print ("average equals " + str(average)) 
+
+print ("moreAverage equals " + str(moreAverage)) 
